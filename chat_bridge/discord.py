0a81f8f5c8e575a75f87877316a0fc726b0f9010
OatmealDome <julian@oatmealdome.me>
2026-02-12 17:35:49 -0500

    discord: Relay IRC messages to Discord

--- chat_bridge/discord.py
+++ chat_bridge/discord.py
@@ -17,6 +17,47 @@ class Bot(Client):
         super(Bot, self).__init__(intents=intents)
         self.cfg = cfg
 
+    def format_irc_message(self, msg):
+        """
+        Turns an IRC message into formatted text for Discord.
+        """
+        chunklist = Tags.parse(msg)
+
+        ret = ""
+        for chunk in chunklist.children:
+            text = chunk.text
+            # Escape all backslashes first.
+            text = text.replace("\\", "\\\\")
+            # Escape the < character to prevent Discord from parsing it.
+            for char in ("<",):
+                text = text.replace(char, "\\" + char)
+
+            if "reset" in chunk.tags:
+                ret += text
+                continue
+
+            if "bold" in chunk.tags:
+                text = "**" + text + "**"
+            if "monospace" in chunk.tags:
+                text = "`" + text + "`"
+            if "italics" in chunk.tags:
+                text = "*" + text + "*"
+            if "strikethrough" in chunk.tags:
+                text = "~~" + text + "~~"
+            if "underline" in chunk.tags:
+                text = "__" + text + "__"
+
+            ret += text
+
+        return ret
+
+    def relay_irc_message(self, who, what):
+        formatted_msg = "**<%s>** %s" % (who, self.format_irc_message(what))
+
+        channel = self.get_channel(self.cfg.channel)
+        f = asyncio.run_coroutine_threadsafe(channel.send(formatted_msg), self.loop)
+        f.result()
+
 
 class EventTarget(events.EventTarget):
     def __init__(self, bot):
@@ -27,13 +68,16 @@ class EventTarget(events.EventTarget):
         self.queue.put(evt)
 
     def accept_event(self, evt):
-        accepted_types = []
+        accepted_types = [events.IRCMessage.TYPE]
         return evt.type in accepted_types
 
     def run(self):
         while True:
             evt = self.queue.get()
-            logging.error("Got unknown event for discord: %r" % evt.type)
+            if evt.type == events.IRCMessage.TYPE:
+                self.bot.relay_irc_message(evt.who, evt.what)
+            else:
+                logging.error("Got unknown event for discord: %r" % evt.type)
 
 
 def start():
